d6fe588a82dca5f29b129c1792c3b63eaf650bcf
ccebreros1 <cesar.cebreros10@gmail.com>
2016-10-04 16:33:16 -0600

    Added POCOs and DTOs

---
+++ ClassLibrary2/DTOs/MenuCategoryFoodItemsDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eRestaurantSystem.Data.POCOs;
+#endregion
+
+//No annotations since this is not an entity
+
+namespace eRestaurantSystem.Data.DTOs
+{
+    public class MenuCategoryFoodItemsDTO
+    {
+        public string MenuCategoryDescription { get; set; }
+        public List<FoodItemCount> FoodItems { get; set; }
+    }
+        
+}
---
+++ ClassLibrary2/POCOs/MenuCategoryFoodItemsPOCO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//No annotations since this is not an entity
+namespace eRestaurantSystem.Data.POCOs
+{
+    public class MenuCategoryFoodItemsPOCO
+    {
+        public string MenuCategoryDescription { get; set; }
+		public int ItemID { get; set;}
+		public string FoodDescription { get; set; }
+		public double CurrentPrice { get; set; }
+		public int TimesServed { get; set; }
+    }
+}
